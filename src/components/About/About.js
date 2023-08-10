204ffcfccc98a4c566c222c2d0ddaf14f845d102
Feromond <Jacobpmish@gmail.com>
2023-08-10 13:07:07 -0600

    Added animation to education page

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -32,7 +32,7 @@ const About = () => (
     <Grid item xs={8} md={7} justifyContent={"center"} alignItems={"center"}>
         <Paper elevation={4} sx={{ padding: '50px' }}>
           <Typography variant="h2" sx={{}}>About Me</Typography>
-          <Typography variant="h6">
+          <Typography variant="h7">
             <p>I am a passionate data professional with a strong focus on machine learning and data science. My educational background in geophysics and computer science has provided me with a unique perspective in tackling complex data-related challenges. Proficient in programming languages like Python, Java, and Rust, and experienced in using libraries such as Numpy, Pandas, Tensorflow, and Pytorch. My expertise includes developing ML models, creating efficient data pipelines, and utilizing cloud computing platforms like AWS and Azure. I have a strong track record of success, including predicting rock permeability with 94% accuracy and detecting geohazards using CNNs. With excellent communication and collaboration skills, I am able to effectively convey complex technical information and work well with cross-functional teams in a diverse range of projects.</p>
 
             <p>I enjoy working on small passion projects in my free time, typically while also trying to learn some sort of new tool. I am constantly learning and trying new things and will hopefully develop some cool projects in the future.</p>
--- src/components/Education/Education.js
+++ src/components/Education/Education.js
@@ -2,47 +2,59 @@ import React from 'react';
 import { Container, Typography, Grid, Paper, List, ListItem, ListItemText, ListItemIcon } from '@mui/material';
 import SchoolIcon from '@mui/icons-material/School';
 import UniversityIcon from '@mui/icons-material/AccountBalance';
+import { useSpring, animated } from 'react-spring';
 
-const Education = () => (
-  <Container sx={{ marginTop: '30px' }}>
-    <Typography variant="h2" sx={{ marginBottom: '20px'}}>Education</Typography>
+const Education = () => {
+  // Define the animation properties
+  const props = useSpring({
+    from: { opacity: 0, transform: 'translate3d(0,-40px,0)' },
+    to: { opacity: 1, transform: 'translate3d(0,0px,0)' },
+    delay: 160
+  });
 
-    <Grid container spacing={3}>
-      <Grid item xs={12} md={6}>
-        <Paper elevation={3} sx={{ padding: '20px' }}>
-          <ListItemIcon>
-            <UniversityIcon color="primary" style={{ fontSize: 50 }} />
-          </ListItemIcon>
-          <Typography variant="h4" sx={{ fontWeight: '500' }}>University Of Calgary</Typography>
-          <Typography variant="subtitle1" sx={{ fontWeight: '400', marginBottom: '10px' }}>Calgary, AB:    September 2018 - May 2022</Typography>
-          <Typography variant="h6" sx={{ fontWeight: '400', marginBottom: '10px' }}>Bachelor of Science in Geophysics</Typography>
-          <List>
-            <ListItem><ListItemText>GPA: 3.7/4.0 Last 90 Credits</ListItemText></ListItem>
-            <ListItem><ListItemText>Graduated With Distinction</ListItemText></ListItem>
-            <ListItem><ListItemText>Elected as Geophysics Representative of the Graduating Class of 2022</ListItemText></ListItem>
-            <ListItem><ListItemText>Suncor Energy Emerging Leaders Science Scholarship</ListItemText></ListItem>
-            <ListItem><ListItemText>(PURE) Program for Undergraduate Research Experience Award</ListItemText></ListItem>
-            <ListItem><ListItemText>Faculty of Science Deans List</ListItemText></ListItem>
-            <ListItem><ListItemText>Jason Lang Scholarship</ListItemText></ListItem>
-          </List>
-        </Paper>
-      </Grid>
+  return (
+    <Container sx={{ marginTop: '30px' }}>
+      <Typography variant="h2" sx={{ marginBottom: '20px'}}>Education</Typography>
 
-      <Grid item xs={12} md={6}>
-        <Paper elevation={3} sx={{ padding: '20px' }}>
-          <ListItemIcon>
-            <SchoolIcon color="primary" style={{ fontSize: 50 }} />
-          </ListItemIcon>
-          <Typography variant="h4" sx={{ fontWeight: '500' }}>St.Mary’s Highschool</Typography>
-          <Typography variant="subtitle1" sx={{ fontWeight: '400', marginBottom: '10px' }}>Calgary, AB - Graduated May 2018</Typography>
-          <Typography variant="h6" sx={{ fontWeight: '400', marginBottom: '10px' }}>High School Diploma</Typography>
-          <List>
-          <ListItem><ListItemText>Full Alexander Rutherford Scholarship</ListItemText></ListItem>
-          </List>
-        </Paper>
-      </Grid>
-    </Grid>
-  </Container>
-);
+      <animated.div style={props}>
+        <Grid container spacing={3}>
+          <Grid item xs={12} md={6}>
+            <Paper elevation={3} sx={{ padding: '20px' }}>
+              <ListItemIcon>
+                <UniversityIcon color="primary" style={{ fontSize: 50 }} />
+              </ListItemIcon>
+              <Typography variant="h4" sx={{ fontWeight: '500' }}>University Of Calgary</Typography>
+              <Typography variant="subtitle1" sx={{ fontWeight: '400', marginBottom: '10px' }}>Calgary, AB:    September 2018 - May 2022</Typography>
+              <Typography variant="h6" sx={{ fontWeight: '400', marginBottom: '10px' }}>Bachelor of Science in Geophysics</Typography>
+              <List>
+                <ListItem><ListItemText>GPA: 3.7/4.0 Last 90 Credits</ListItemText></ListItem>
+                <ListItem><ListItemText>Graduated With Distinction</ListItemText></ListItem>
+                <ListItem><ListItemText>Elected as Geophysics Representative of the Graduating Class of 2022</ListItemText></ListItem>
+                <ListItem><ListItemText>Suncor Energy Emerging Leaders Science Scholarship</ListItemText></ListItem>
+                <ListItem><ListItemText>(PURE) Program for Undergraduate Research Experience Award</ListItemText></ListItem>
+                <ListItem><ListItemText>Faculty of Science Deans List</ListItemText></ListItem>
+                <ListItem><ListItemText>Jason Lang Scholarship</ListItemText></ListItem>
+              </List>
+            </Paper>
+          </Grid>
+
+          <Grid item xs={12} md={6}>
+            <Paper elevation={3} sx={{ padding: '20px' }}>
+              <ListItemIcon>
+                <SchoolIcon color="primary" style={{ fontSize: 50 }} />
+              </ListItemIcon>
+              <Typography variant="h4" sx={{ fontWeight: '500' }}>St.Mary’s Highschool</Typography>
+              <Typography variant="subtitle1" sx={{ fontWeight: '400', marginBottom: '10px' }}>Calgary, AB - Graduated May 2018</Typography>
+              <Typography variant="h6" sx={{ fontWeight: '400', marginBottom: '10px' }}>High School Diploma</Typography>
+              <List>
+                <ListItem><ListItemText>Full Alexander Rutherford Scholarship</ListItemText></ListItem>
+              </List>
+            </Paper>
+          </Grid>
+        </Grid>
+      </animated.div>
+    </Container>
+  );
+};
 
 export default Education;
