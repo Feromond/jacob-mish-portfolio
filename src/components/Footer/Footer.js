489506d554b4cb03ceee6aeb86f26ff9288ff585
Feromond <Jacobpmish@gmail.com>
2023-08-17 11:06:54 -0600

    Added tooltips to footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container, IconButton, Link, Grid, Typography, Box } from '@mui/material';
+import { Container, IconButton, Link, Grid, Typography, Box, Tooltip} from '@mui/material';
 import LinkedInIcon from '@mui/icons-material/LinkedIn';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import EmailIcon from '@mui/icons-material/Email';
@@ -11,32 +11,40 @@ const Footer = () => {
       <Container>
         <Grid container spacing={4} justifyContent="center" alignItems="center">
           <Grid item>
+          <Tooltip title="My Linkedin Profile">
             <IconButton color="primary">
               <Link href="https://www.linkedin.com/in/jacob-mish-25915722a/" target="_blank" rel="noopener">
                 <LinkedInIcon />
               </Link>
             </IconButton>
+            </Tooltip>
           </Grid>
           <Grid item>
+          <Tooltip title="My Github Profile">
             <IconButton color="primary">
               <Link href="https://github.com/Feromond" target="_blank" rel="noopener">
                 <GitHubIcon />
               </Link>
             </IconButton>
+            </Tooltip>
           </Grid>
           <Grid item>
+          <Tooltip title="My Email">
             <IconButton color="primary">
               <Link href="mailto:JacobPMish@gmail.com" target="_blank" rel="noopener">
                 <EmailIcon />
               </Link>
             </IconButton>
+            </Tooltip>
           </Grid>
           <Grid item>
+          <Tooltip title="My Phone Number">
             <IconButton color="primary">
               <Link href="tel:+1-403-836-9391" target="_blank" rel="noopener">
                 <PhoneIcon />
               </Link>
             </IconButton>
+            </Tooltip>
           </Grid>
         </Grid>
         <Typography variant="body2" align="center" sx={{ marginTop: '10px' }}>© Jacob Mish 2023</Typography>
